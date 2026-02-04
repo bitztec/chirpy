eed88ff293cc76f47b8c87600dd81005b6624ca9
can-ek <can-ek@github.com>
2026-02-03 20:03:49 -0800

    Adding custom handler for server health

--- main.go
+++ main.go
@@ -12,6 +12,15 @@ func main() {
 		Handler: serverMux,
 	}
 
-	serverMux.Handle("/", http.FileServer(http.Dir("./")))
+	serverMux.Handle(
+		"/app/",
+		http.StripPrefix("/app", http.FileServer(http.Dir("./"))))
+	serverMux.HandleFunc("/healthz", hearthBeatHandler)
 	log.Fatal(server.ListenAndServe())
+}
+
+func hearthBeatHandler(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(200)
+	writer.Write([]byte("OK"))
 }
