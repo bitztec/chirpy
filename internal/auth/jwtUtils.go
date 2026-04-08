afbe5bd2b4227e5805c3468de1e124bee21d7c0c
can-ek <can-ek@github.com>
2026-04-07 20:05:44 -0700

    Adding capability to create jwt tokens for authentication

---
+++ internal/auth/jwtUtils.go
@@ -0,0 +1,50 @@
+package auth
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	subject := userID.String()
+	signingKey := []byte(tokenSecret)
+	claims := jwt.RegisteredClaims{
+		Issuer:    "chirpy-access",
+		IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn).UTC()),
+		Subject:   subject,
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(signingKey)
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claims := jwt.RegisteredClaims{}
+	token, err := jwt.ParseWithClaims(
+		tokenString,
+		&claims,
+		func(token *jwt.Token) (interface{}, error) { return []byte(tokenSecret), nil })
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	subject, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	issuer, err := token.Claims.GetIssuer()
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	if issuer != "chirpy-access" {
+		return uuid.Nil, errors.New("invalid issuer")
+	}
+
+	return uuid.Parse(subject)
+}
