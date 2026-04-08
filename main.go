e31c9e3356a2e6226834a81b357f9e3be7f14870
can-ek <can-ek@github.com>
2026-04-07 20:06:01 -0700

    clean up unused code

--- main.go
+++ main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"strings"
 	"sync/atomic"
 
 	"github.com/bitztec/chirpy/internal/database"
@@ -63,68 +61,4 @@ func hearthBeatHandler(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	writer.WriteHeader(200)
 	writer.Write([]byte("OK"))
-}
-
-func validateChirpHandler(writer http.ResponseWriter, request *http.Request) {
-	type parameters struct {
-		Body string `json:"body"`
-	}
-
-	type validationResult struct {
-		Error   string `json:"error"`
-		Body    string `json:"cleaned_body"`
-		isValid bool
-	}
-
-	decoder := json.NewDecoder(request.Body)
-	params := parameters{}
-	err := decoder.Decode(&params)
-	if err != nil {
-		fmt.Printf("\nError decoding parameters: %s", err)
-		writer.WriteHeader(500)
-		return
-	}
-
-	respBody := validationResult{}
-
-	if len(params.Body) > 140 {
-		respBody.Error = "Chirp is too long"
-	} else {
-		respBody.Body = CleanResponse(params.Body)
-		respBody.isValid = true
-	}
-
-	dat, err := json.Marshal(respBody)
-	if err != nil {
-		fmt.Printf("\nError marshalling JSON: %s", err)
-		writer.WriteHeader(500)
-		return
-	}
-
-	if respBody.isValid {
-		writer.WriteHeader(200)
-	} else {
-		writer.WriteHeader(400)
-	}
-
-	writer.Header().Set("Content-Type", "application/json")
-	writer.Write(dat)
-}
-
-func CleanResponse(body string) string {
-	parts := strings.Split(body, " ")
-	newParts := make([]string, len(parts))
-
-	for i, word := range parts {
-		lower := strings.ToLower(word)
-		if lower == "kerfuffle" ||
-			lower == "sharbert" ||
-			lower == "fornax" {
-			word = "****"
-		}
-
-		newParts[i] = word
-	}
-
-	return strings.Join(newParts, " ")
 }
